21827526ec590f13188c62b3858419c5236bbeb0
liu1597 <48242495+liu1597@users.noreply.github.com>
2024-07-19 01:51:29 +0930

    oath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,22 @@ import Image from "next/image";
 import Hero from "@/app/components/Hero";
 import Jobs from "./components/Jobs";
 
-export default function Home() {
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from '@workos-inc/authkit-nextjs';
+
+export default async function Home() {
+    // Retrieves the user from the session or returns `null` if no user is signed in
+    const { user } = await getUser();
+
+    // Get the URL to redirect the user to AuthKit to sign in
+    const signInUrl = await getSignInUrl();
+  
+    // Get the URL to redirect the user to AuthKit to sign up
+    const signUpUrl = await getSignUpUrl();
   return (
     <>
    <Hero />
