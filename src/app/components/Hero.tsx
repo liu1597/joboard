2a31133ff8c1ea7d7ec327dfde4dd5d65dbd9449
jian-qin <48242495+liu1597@users.noreply.github.com>
2024-07-20 04:20:58 +0930

    Revert "Feat"

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,4 @@
-export default async function Hero() {
-
+export default function Hero() {
   return (
     <section className="container my-16">
       <h1 className="text-4xl font-bold text-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,7 @@ import Image from "next/image";
 import Hero from "@/app/components/Hero";
 import Jobs from "./components/Jobs";
 
-import {
-  getSignInUrl,
-  getSignUpUrl,
-  getUser,
-  signOut,
-} from '@workos-inc/authkit-nextjs';
-
-export default async function Home() {
-    // Retrieves the user from the session or returns `null` if no user is signed in
-    const { user } = await getUser();
-
-    // Get the URL to redirect the user to AuthKit to sign in
-    const signInUrl = await getSignInUrl();
-  
-    // Get the URL to redirect the user to AuthKit to sign up
-    const signUpUrl = await getSignUpUrl();
+export default function Home() {
   return (
     <>
    <Hero />
